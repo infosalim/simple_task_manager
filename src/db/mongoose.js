a67052bda5fdfba4abd673ecb94c3191dffdcd86
infosalim <salimh5757@gmail.com>
2019-09-29 00:15:36 +0600

    written some validation code

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const validator = require('validator');
 
 mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
     useNewUrlParser: true,
@@ -8,20 +9,75 @@ mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
 
 const User = mongoose.model('User', {
     name: {
-        type: String
+        type: String,
+        required: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        trim: true,
+        lowercase: true,
+        validate(value) {
+            if (!validator.isEmail(value)) {
+                throw new Error('Email is invalid');
+            }
+        }
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 8,
+        trim: true,
+        validate(value){
+            if(value.toLowerCase().includes('password')){
+                throw new Error('Password can\'t contain "password"');
+            }
+        }
     },
     age: {
-        type: Number
+        type: Number,
+        default: 1,
+        validate(value) {
+            if (value < 1) {
+                throw new Error('Age must be a positive number and greater than 0');
+            }
+        }
     }
 });
 
-const salim = new User({
-    name: 'Salim Hossain',
-    age: 22
+// const salim = new User({
+//     name: 'Sagor Ahmed',
+//     age: 16,
+//     email: 'sagorAhmeD@gmail.com',
+//     password: 'milasSalim'
+// });
+
+// salim.save().then(() => {
+//     console.log(salim);
+// }).catch(error => {
+//     console.log(error.message);
+// });
+
+const Task = mongoose.model('Task', {
+    description: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    completed: {
+        type: Boolean,
+        default: false
+    }
 });
 
-salim.save().then(()=>{
-    console.log(salim);
-}).catch(error => {
+const cooking = new Task({
+    description: 'Beefff!!',
+    completed: true,
+});
+
+cooking.save().then(()=>{
+    console.log(cooking);
+}).catch(error=>{
     console.log(error.message);
 });
