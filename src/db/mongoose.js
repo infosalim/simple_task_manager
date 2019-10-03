3ffb09e47ce7211b87206d69e7fc13de8f8bd5d6
infosalim <salimh5757@gmail.com>
2019-10-03 22:26:34 +0600

    settuped besic project structure

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,63 +1,10 @@
 const mongoose = require('mongoose');
-const validator = require('validator');
 
 mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
     useNewUrlParser: true,
     useCreateIndex: true,
     useUnifiedTopology: true
 });
-
-const User = mongoose.model('User', {
-    name: {
-        type: String,
-        required: true,
-        trim: true
-    },
-    email: {
-        type: String,
-        required: true,
-        trim: true,
-        lowercase: true,
-        validate(value) {
-            if (!validator.isEmail(value)) {
-                throw new Error('Email is invalid');
-            }
-        }
-    },
-    password: {
-        type: String,
-        required: true,
-        minlength: 8,
-        trim: true,
-        validate(value){
-            if(value.toLowerCase().includes('password')){
-                throw new Error('Password can\'t contain "password"');
-            }
-        }
-    },
-    age: {
-        type: Number,
-        default: 1,
-        validate(value) {
-            if (value < 1) {
-                throw new Error('Age must be a positive number and greater than 0');
-            }
-        }
-    }
-});
-
-// const salim = new User({
-//     name: 'Sagor Ahmed',
-//     age: 16,
-//     email: 'sagorAhmeD@gmail.com',
-//     password: 'milasSalim'
-// });
-
-// salim.save().then(() => {
-//     console.log(salim);
-// }).catch(error => {
-//     console.log(error.message);
-// });
 
 const Task = mongoose.model('Task', {
     description: {
@@ -69,15 +16,4 @@ const Task = mongoose.model('Task', {
         type: Boolean,
         default: false
     }
-});
-
-const cooking = new Task({
-    description: 'Beefff!!',
-    completed: true,
-});
-
-cooking.save().then(()=>{
-    console.log(cooking);
-}).catch(error=>{
-    console.log(error.message);
 });
---
+++ src/index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+require('./db/mongoose');
+const User = require('./models/user');
+
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.post('/users', (req, res)=>{
+    const user = new User(req.body);
+    console.log(user);
+    user.save().then(() => {
+        res.send(user);
+    }).catch(error => {
+        console.log(error.message);
+    })
+});
+
+
+app.listen(port, ()=>{
+    console.log(`Server is up on port ${port}`);
+});
