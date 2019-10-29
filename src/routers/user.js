2a770ca06bb09179a35ba6620d4c88bf641876c2
infosalim <salimh5757@gmail.com>
2019-10-29 22:49:49 +0600

    served the files from server

--- src/routers/user.js
+++ src/routers/user.js
@@ -10,41 +10,41 @@ router.post('/users', async (req, res) => {
     try {
         await user.save();
         const token = await user.generateAuthToken();
-        res.status(201).send({ user, token});
+        res.status(201).send({ user, token });
     } catch (e) {
         res.status(400).send(e.message);
     }
 });
 
 router.post('/users/login', async (req, res) => {
-    try{
+    try {
         const user = await User.findByCredentials(req.body.email, req.body.password);
         const token = await user.generateAuthToken();
-        res.send({user, token});
-    }catch(e){
+        res.send({ user, token });
+    } catch (e) {
         res.status(400).send();
     }
 });
 
-router.post('/users/logout', auth, async (req, res)=>{
-    try{
+router.post('/users/logout', auth, async (req, res) => {
+    try {
         req.user.tokens = req.user.tokens.filter(token => {
             return token.token !== req.token;
         });
 
         await req.user.save();
         res.send();
-    }catch(e){
+    } catch (e) {
         res.status(500).send();
     }
 });
 
-router.post('/users/logoutAll', auth, async (req, res)=>{
-    try{
+router.post('/users/logoutAll', auth, async (req, res) => {
+    try {
         req.user.tokens = [];
         await req.user.save();
         res.send();
-    }catch(e){
+    } catch (e) {
         res.status(500).send();
     }
 })
@@ -86,8 +86,8 @@ const upload = multer({
     limits: {
         fileSize: 1000000
     },
-    fileFilter(req, file, callback){
-        if(!file.originalname.match(/\.(jpg|jpeg|png)$/)){
+    fileFilter(req, file, callback) {
+        if (!file.originalname.match(/\.(jpg|jpeg|png)$/)) {
             return callback(new Error('Please upload a .jpg image.'));
         }
         callback(undefined, true)
@@ -98,7 +98,7 @@ router.post('/users/me/avatar', auth, upload.single('avatar'), async (req, res) 
     req.user.avatar = req.file.buffer;
     await req.user.save();
     res.send()
-}, (error, req, res, next)=>{
+}, (error, req, res, next) => {
     res.status(400).send({ error: error.message });
 });
 
@@ -106,6 +106,20 @@ router.delete('/users/me/avatar', auth, async (req, res) => {
     req.user.avatar = undefined;
     await req.user.save();
     res.send();
+});
+
+router.get('/users/:id/avatar', async (req, res) => {
+    try {
+        const user = await User.findById(req.params.id);
+
+        if (!user || !user.avatar) {
+            throw new Error();
+        }
+        res.set('Content-Type', 'image/jpg');
+        res.send(user.avatar);
+    } catch (e) {
+        res.status(400).send();
+    }
 })
 
 
